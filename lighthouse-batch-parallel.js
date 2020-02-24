a663fdec3d832a5f11c8e6e457b56970df4e2db3
Carr1005 <carr.z.wang@gmail.com>
2020-02-25 00:26:59 +0800

    Provide more available parameters for command line tool

--- lighthouse-batch-parallel.js
+++ lighthouse-batch-parallel.js
@@ -1,26 +1,125 @@
 #!/usr/bin/env node
-
-const lighthouseGenuis = require('.');
+const fs = require('fs');
+const { lighthouseBatchParallel } = require('.');
 const program = require('commander');
 
-const { throttlingOptions } = require('./assets/defaultConfig.js');
+const {
+  createOutputDir,
+  consoleLogger,
+  errorLogger,
+  calculateElapsedTime,
+} = require('./utils.js');
+
+const {
+  throttlingOptions,
+  supportedOutputFileTypes,
+} = require('./assets/defaultConfig.js');
 
 program
   .option('-n, --number <number>', 'Number of workers')
   .option('-a, --audits-config <path>', 'Custom audits config')
   .option('-t, --throttling <method>', 'Throttling Method')
-  .option('-l, --log-mode', 'Log progress of process');
+  .option('-p, --path <path>', 'The location of output file')
+  .option('-f, --file-name <name>', 'The name of output file')
+  .option('-o, --output-format <format>', 'The output format')
+  .option('-l, --log-mode', 'Log progress of process')
+  .option('-e, --error-log-file', 'Record error');
 
 program.parse(process.argv);
 
 if (program.throttling && !throttlingOptions.includes(program.throttling)) {
-  console.log(`Cannot understand provided throttling method, please provide property in one of [ ${throttlingOptions} ]`);
-} else {
-  lighthouseGenuis({ 
-    inputFilePath:        program.args[0],
-    workersNum:           program.number,
-    customAuditsFilePath: program.auditsConfig,
-    throttling:           program.throttling,
-    logMode:              program.logMode,
-  });
+  console.log(`Cannot process provided throttling method, please provide property in one of [ ${throttlingOptions} ]`);
+  return;
 }
+
+const defaultErrLogDirName = 'errorLog';
+const clock = new Date();
+const genDateTime = new Date(clock.getTime() - (clock.getTimezoneOffset() * 60000)).toISOString().slice(0, 19);
+
+let outputPath;
+let outputFileName;
+let outputFileFormat;
+let writeStream;
+let errLogWriteStream = {};
+
+if (program.path) {
+  createOutputDir(program.path);
+  outputPath = program.path;
+} else {
+  const defaultOutputDirName = 'output';
+  createOutputDir(defaultOutputDirName);
+  outputPath = `./${defaultOutputDirName}`;
+}
+
+if (program.fileName) {
+  outputFileName = program.fileName.split('.')[0];
+  const fileFormat = program.fileName.split('.')[1];
+  if (fileFormat) {
+    outputFileFormat = fileFormat;
+  }
+} else {
+  outputFileName = genDateTime;
+}
+
+if (program.outputFormat && !outputFileFormat) {
+  outputFileFormat = program.outputFormat;
+}
+
+if (outputFileFormat && !supportedOutputFileTypes.includes(outputFileFormat)) {
+  console.log(`Cannot process provided file format for output file, please provide format in one of [ ${supportedOutputFileTypes} ]`);
+  return;
+}
+
+if (!outputFileFormat) {
+  outputFileFormat = supportedOutputFileTypes[0];
+}
+
+writeStream = fs.createWriteStream(`${outputPath}/${outputFileName}.${outputFileFormat}`, { flags: 'ax' });
+
+writeStream.on('error', (error) => {
+  console.log(error);
+});
+
+writeStream.on('open', () => {
+  const startTime = Date.now();
+  if (outputFileFormat === supportedOutputFileTypes[1]) {
+    writeStream.write('[');
+  }
+
+  if (program.errorLogFile) {
+    createOutputDir(defaultErrLogDirName);
+    errLogWriteStream = fs.createWriteStream(`./${defaultErrLogDirName}/${genDateTime}-error-log.${outputFileFormat}`, { flags: 'ax' });  
+  }
+
+  const lighthouseAuditing = lighthouseBatchParallel({
+    input:        program.args[0],
+    customAuduts: program.auditsConfig,
+    throttling:   program.throttling,
+    outputFormat: outputFileFormat,
+    workersNum:   program.number,
+  });
+
+  lighthouseAuditing.on('data', ({ data, progress, header }) => {
+    const { device, url, totalTasksNum, leftTasksNum } = progress;
+    consoleLogger(program.logMode && !header, `Device = ${device} || URL = ${url}`);  
+    consoleLogger(program.logMode, `Total: ${totalTasksNum} || Remain: ${leftTasksNum}`);  
+    if (outputFileFormat === supportedOutputFileTypes[1] && leftTasksNum !== 0) {
+      data+=',';
+    }
+    writeStream.write(data);
+  });
+
+  lighthouseAuditing.on('error', ({ error }) => {
+    errorLogger(program.errorLogFile, () => errLogWriteStream.write(error));
+  });
+
+  lighthouseAuditing.on('end', () => {
+    const { seconds, minutes } = calculateElapsedTime(startTime);
+    consoleLogger(program.logMode, `Total time to accomplish all tasks: ${minutes} minutes ${seconds} seconds`); 
+    if (outputFileFormat === supportedOutputFileTypes[1]) {
+      writeStream.write(']');
+    }
+    writeStream.end();
+    errorLogger(program.errorLogFile, () => errLogWriteStream.end());
+  });
+});
--- utils.js
+++ utils.js
@@ -3,16 +3,22 @@ const fs = require('fs');
 module.exports.createOutputDir = (dir) => {
   try {
     if (!fs.existsSync(dir)){
-      fs.mkdirSync(dir)
+      fs.mkdirSync(dir, { recursive: true });
     }
   } catch (err) {
     console.error(err)
   }
 }
 
-module.exports.logger = (logMode, message) => {
+module.exports.consoleLogger = (logMode, message) => {
   if (logMode) {
     console.log(message);
+  }
+}
+
+module.exports.errorLogger = (logMode, behavior) => {
+  if (logMode) {
+    behavior();
   }
 }
 
@@ -22,7 +28,7 @@ module.exports.calculateElapsedTime = (startTime) => {
   const seconds = Math.floor(totalTimeInSeconds % 60);
   const minutes = Math.floor(totalTimeInSeconds / 60);
   return {
-  	seconds,
-  	minutes,
+    seconds,
+    minutes,
   };
 }
